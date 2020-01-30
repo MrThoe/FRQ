94c082404e4fa7172091676f46f37b65e057cc23
Allen Thoe <30870017+MrThoe@users.noreply.github.com>
2020-01-30 13:42:51 -0800

    Create APCalendar.java

---
+++ 2019/APCalendar.java
@@ -0,0 +1,48 @@
+public class APCalendar
+{
+	 /** Returns true if year is a leap year and false otherwise. */
+	 private static boolean isLeapYear(int year){ 
+		 /* implementation not shown */ 
+
+		 return false;
+	 }
+	 /** Returns the number of leap years between year1 and year2, inclusive.
+	 * Precondition: 0 <= year1 <= year2
+	 */
+	 public static int numberOfLeapYears(int year1, int year2)
+	 { /* to be implemented in part (a) */ 
+
+		 return 0;
+	 }
+	 
+	 private static int firstDayOfYear(int year)
+	 { /* implementation not shown */ 
+		 
+		return 0;
+	 }
+	 
+	 
+	 /** Returns n, where month, day, and year specify the nth day of the year.
+	 * Returns 1 for January 1 (month = 1, day = 1) of any year.
+	 * Precondition: The date represented by month, day, year is a valid date.
+	 */
+	 
+	 
+	 
+	 private static int dayOfYear(int month, int day, int year)
+	 { /* implementation not shown */ 
+		 
+		 return 0;
+	 }
+	 /** Returns the value representing the day of the week for the given date
+	 * (month, day, year), where 0 denotes Sunday, 1 denotes Monday, ...,
+	 * and 6 denotes Saturday.
+	 * Precondition: The date represented by month, day, year is a valid date.
+	 */
+	 public static int dayOfWeek(int month, int day, int year)
+	 { /* to be implemented in part (b) */ 
+		 
+		 return 0;
+	 }
+	 // There may be instance variables, constructors, and other methods not shown.
+}
