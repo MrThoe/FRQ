d477b5e2626fd6fea1b88b6f365f3b6cf70abcc3
Allen Thoe <30870017+MrThoe@users.noreply.github.com>
2020-02-10 13:55:38 -0800

    Update FrogSimulation.java

--- 2018/FrogSimulation.java
+++ 2018/FrogSimulation.java
@@ -9,13 +9,16 @@ public class FrogSimulation {
 		maxHops = numHops;
 	}
 	
+	/*There are no parameters or description of how this works so
+	 * I just made my own (-15,15) which isn't correct since in one of the 
+	 * simulations it jumped 31.  But it appears as an outlier so....
+	 */
 	private int hopDistance() {
-		return 0;
+		return (int)Math.random()*30 - 15;
 	}
 	
 	private boolean simulate() {
 		//PART A
-		int count = maxHops;
 		for(int i = 0; i < maxHops; i++) {
 			goalDistance -= hopDistance();
 			if(goalDistance <= 0) {
@@ -27,9 +30,14 @@ public class FrogSimulation {
 	
 	public double runSimulations(int num) {
 		//PART B
-		
+		double count = 0;
+		for(int i = 0; i < num; i++) {
+			if(simulate()) {
+				count++;
+			}
+		}
+		count /= num;
+		return count;
 	}
 	
 }
-
-
