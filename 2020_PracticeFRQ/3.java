76e250d1c78cd3e577f33b895b336d5a4a35b6d3
Allen Thoe <30870017+MrThoe@users.noreply.github.com>
2022-04-26 15:09:34 -0700

    Create 3.java

---
+++ 2020_PracticeFRQ/3.java
@@ -0,0 +1,15 @@
+public void addReview(ProductReview prodReview)
+{
+  reviewList.add(prodReview);  
+  productList.add(prodReview.getName());
+}
+
+public int getNumGoodReviews(String prodName)
+{
+  int count = 0;
+  for(int i = 0; i < prodReview.length(); i++)
+  {
+    if(prodReview.get(i).getReview #TODO TOMORRWO)) !=   
+  }
+  return count;
+}
